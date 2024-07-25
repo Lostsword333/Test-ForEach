87de780aaa236f6a758c1ce3f1d69e75c2c603c3
Lostsword333 <125037471+Lostsword333@users.noreply.github.com>
2024-07-25 14:01:58 +0000

    App FEA

--- excuses-de-dev-frontend/src/app/error/error.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-error',
-  template: `<h1>404 - Page not found</h1>`,
-  styleUrls: ['./error.component.css']
-})
-export class ErrorComponent { }
